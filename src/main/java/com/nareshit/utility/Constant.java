229263b158378bd87b995ee1d9235fb037f1595d
Shahnawaz <shahnawazshaikh6200@gmail.com>
2025-09-25 01:11:33 +0530

    Initial commit: E-Commerce BookStore

---
+++ src/main/java/com/nareshit/utility/Constant.java
@@ -0,0 +1,10 @@
+package com.nareshit.utility;
+
+public class Constant {
+	
+	public static final String SUCCESS ="success";
+	public static final String FAILURE ="failure";
+	public static final String FAILED ="failed";
+	
+
+}
